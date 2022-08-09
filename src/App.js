7757c28ef94056ba0b1555e43b73ee0275e2b895
princewill-opah <98644246+princewill-opah@users.noreply.github.com>
2022-08-09 14:29:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello young mann, Welcome to the {value}</div>;
 }
 
 export default App;
