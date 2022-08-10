95bd7bff3c8e2f36f457a545016c7c3032aaa2ee
princewill-opah <98644246+princewill-opah@users.noreply.github.com>
2022-08-10 17:28:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'My World';
+  const value = 'My very World';
   return <div>Hello young mann, Welcome to the {value} {process.env.ECHO_XX}</div>;
 }
 
